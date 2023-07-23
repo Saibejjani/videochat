eb161ae426c37bf0ef8b6fa1da17ba38d3dc0ad7
Saibejjani <64737059+Saibejjani@users.noreply.github.com>
2023-07-23 23:02:54 +0530

    intial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { Server, Socket } = require('socket.io');
+const bodyParser = require('body-parser');
+
+const io = new Server();
+const app = express();
+
+app.use(bodyParser);
+
+io.on('connection', (socket) => {});
+
+app.listen(8000, () => {
+  console.log('Http server running at port 8000');
+});
+
+io.listen(8001, () => {
+  console.log('socket running at port 8001');
+});
